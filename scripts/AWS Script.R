5a52ad68201c7dc8a7f062067c9be6064a6b8025
Blake Shurtz <blakeobeans@gmail.com>
2020-02-01 00:29:14 -0800

    added AWS script

---
+++ scripts/AWS Script.R
@@ -0,0 +1,19 @@
+remove.packages("rstan")
+remove.packages("StanHeaders")
+install.packages("rstan", type = "source")
+library(rstan)
+library(StanHeaders)
+rstan_options(auto_write = TRUE) #cache model
+options(mc.cores = parallel::detectCores()) #multiple cores for multiple chains
+load("~/brms_data.rda")
+m1 <- stanc(file = "bmrs_model.stan")
+m1 <- stan_model(stanc_ret = m1)
+fit1 <- sampling(m1, #use sampling function, rather than stan()
+                 warmup=1000, #
+                 iter=6000, #total iterations including warmup
+                 seed=1, #ensures reproducibility 
+                 data=stan_data, 
+                 chains = 4, #each chain has different initial condition which helps to find pathological neighborhood of posterior
+                 cores = getOption("mc.cores", 1L),
+                 verbose = FALSE,
+                 refresh = 100)
