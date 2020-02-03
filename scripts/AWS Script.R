c8cc3d4011853efffe30a7d76cad07a2dd1629b4
Blake Shurtz <blakeobeans@gmail.com>
2020-02-02 17:41:28 -0800

    commit to aws

--- scripts/AWS Script.R
+++ scripts/AWS Script.R
@@ -8,12 +8,13 @@ options(mc.cores = parallel::detectCores()) #multiple cores for multiple chains
 load("~/brms_data.rda")
 m1 <- stanc(file = "bmrs_model.stan")
 m1 <- stan_model(stanc_ret = m1)
+getOption("mc.cores", 1L)
 fit1 <- sampling(m1, #use sampling function, rather than stan()
-                 warmup=1000, #
-                 iter=6000, #total iterations including warmup
+                 warmup=250, #
+                 iter=1000, #total iterations including warmup
                  seed=1, #ensures reproducibility 
                  data=stan_data, 
                  chains = 4, #each chain has different initial condition which helps to find pathological neighborhood of posterior
                  cores = getOption("mc.cores", 1L),
                  verbose = FALSE,
-                 refresh = 100)
+                 refresh = 1)
